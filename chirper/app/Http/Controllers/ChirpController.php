32e5bc0b67b1ce33358f73735f14abb42ff45ad9
Breno Santos Almeida <breno098526049@gmail.com>
2022-09-29 09:42:35 -0300

    laravel bootcamp chirper

---
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ChirpController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Chirps/Index', [
+						'chirps' =>  Chirp::with('user:id,name')->latest()->get(),
+				]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+				$validated = $request->validate([
+					'message' => 'required|string|max:255',
+				]);
+
+				$request->user()->chirps()->create($validated);
+
+				return redirect(route('chirps.index'));
+    }
+
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+			$this->authorize('update', $chirp);
+ 
+			$validated = $request->validate([
+					'message' => 'required|string|max:255',
+			]);
+
+			$chirp->update($validated);
+
+			return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chirp $chirp)
+    {
+			$this->authorize('delete', $chirp);
+ 
+			$chirp->delete();
+
+			return redirect(route('chirps.index'));
+    }
+}
